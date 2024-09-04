c79d55db07a04bff3020f0b1a996b2b2ac510d90
Roudi22 <roudi.alzkemi@gmail.com>
2024-09-04 20:41:56 +0200

    skapa "game" state som innehåller spelet skapat av två spelare och "grid" state som innehåller Boarde:n

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
-
+import { Game } from './classes/Game';
+import { Player } from './classes/Player';
 import Header from './components/Header';
+import BoardComponent from './components/BoardComponent';
 
 function App() {
-  const [player1Name, setPlayer1Name] = useState('Player 1');
-  const [player2Name, setPlayer2Name] = useState('Player 2');
+  const [player1Name, setPlayer1Name] = useState(new Player('Player 1', 'X'));
+  const [player2Name, setPlayer2Name] = useState(new Player('Player 2', 'O'));
+  const [game, setGame] = useState(new Game(player1Name, player2Name));
+  const [grid, setGrid] = useState(game.getBoard());
+
   return (
     <>
       <Header
         players={[player1Name || 'Player 1', player2Name || 'Player 2']}
       />
+      <BoardComponent grid={grid} onCellClick={() => {}} />
     </>
   );
 }
