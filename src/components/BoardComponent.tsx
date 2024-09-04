56375f117e1f290523249b2868df90ae4cc3c935
Roudi22 <roudi.alzkemi@gmail.com>
2024-09-05 00:09:33 +0200

    Lagt till useEffect för att animera cellen när spelaren gör ett drag

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface BoardComponentProps {
   grid: string[][];
@@ -9,6 +9,22 @@ const BoardComponent: React.FC<BoardComponentProps> = ({
   grid,
   onCellClick,
 }) => {
+  const [animatedCells, setAnimatedCells] = useState<(string | null)[][]>(
+    grid.map((row) => row.map(() => null))
+  );
+
+  useEffect(() => {
+    const newAnimatedCells = grid.map((row, rowIndex) =>
+      row.map((cell, colIndex) => {
+        if (cell && !animatedCells[rowIndex][colIndex]) {
+          return cell; // Mark cell to be animated
+        }
+        return animatedCells[rowIndex][colIndex];
+      })
+    );
+    setAnimatedCells(newAnimatedCells);
+  }, [grid]);
+
   return (
     <div className="flex flex-col items-center mt-5">
       {grid.map((row, rowIndex) => (
@@ -21,10 +37,18 @@ const BoardComponent: React.FC<BoardComponentProps> = ({
             >
               <div className="p-4">
                 {cell === 'X' && (
-                  <div className="w-[40px] rounded-full  h-[40px] bg-red-500"></div>
+                  <div
+                    className={`w-[40px] rounded-full h-[40px] bg-red-500 ${
+                      animatedCells[rowIndex][colIndex] ? 'animate-drop' : ''
+                    }`}
+                  ></div>
                 )}
                 {cell === 'O' && (
-                  <div className="w-[40px] rounded-full h-[40px] bg-yellow-500"></div>
+                  <div
+                    className={`w-[40px] rounded-full h-[40px] bg-yellow-500 ${
+                      animatedCells[rowIndex][colIndex] ? 'animate-drop' : ''
+                    }`}
+                  ></div>
                 )}
               </div>
             </div>
