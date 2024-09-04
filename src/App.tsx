9223b665fbe568b3edefab126e53e2e624ff8ee4
Linus Almgren <linus.almgren@gmail.com>
2024-09-04 22:48:41 +0200

    delete old checkForWin and add mock board to check

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 
 import Header from './components/Header';
+import { WinChecker } from './classes/WinChecker';
+
+const mockBoard = [
+  ['', '', '', '', '', ''],
+  ['', '', '', '', '', ''],
+  ['', '', '', '', '', ''],
+  ['', '', '', '', '', ''],
+  ['', '', 'X', 'X', 'X', 'X'],
+];
 
 function App() {
   const [players, setPlayers] = useState(['Player 1', 'Player 2']);
+  console.log(WinChecker.checkForWin('X', mockBoard));
 
   return (
     <>
