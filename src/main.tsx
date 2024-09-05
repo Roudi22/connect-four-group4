2fe429b5336d8287aad4bca81eae3188568a1d77
AdamLunaAH <adam.a-h@outlook.com>
2024-09-05 15:33:47 +0200

    ErrorBoundary added to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// ErrorBoundary and component
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorComponent from './components/ErrorBoundary.tsx';
+// ErrorBoundary test
+import ErrorThrowingComponent from './components/ErrorThrowing.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ErrorBoundary
+      FallbackComponent={ErrorComponent}
+      onReset={() => location.reload()}
+    >
+      <ErrorThrowingComponent />
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+);
