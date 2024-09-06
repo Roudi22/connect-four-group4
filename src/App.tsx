2434f51fad53f98277f61936343f074fc83ed5bd
Nuur Yusuf <112573333+nyusufdot88@users.noreply.github.com>
2024-09-06 03:07:12 +0200

    GamePoppup component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import GameModePopup from './components/GameModePopup';
 import { Game } from './classes/Game';
 import { Player } from './classes/Player';
 import Header from './components/Header';
@@ -12,6 +12,7 @@ function App() {
   const [game, setGame] = useState(new Game(player1Name, player2Name));
   const [grid, setGrid] = useState(game.getBoard());
   const [message, setMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(true);
 
   const handleCellClick = (col: number) => {
     if (game && game.playTurn(col)) {
@@ -23,11 +24,29 @@ function App() {
       }
     }
   };
+
+  function handleHumanNamesSubmit(
+    player1Name: string,
+    player2Name: string,
+    isAI: boolean
+  ) {
+    const player1 = new Player(player1Name || 'Player 1', 'X');
+    const player2 = new Player(
+      player2Name || (isAI ? 'Computer' : 'Player 2'),
+      'O'
+    );
+    setPlayer1Name(player1);
+    setPlayer2Name(player2);
+    setGame(new Game(player1, player2));
+    setGrid(game.getBoard());
+    setMessage(`${player1.name}'s playTurn`);
+    setShowPopup(false);
+  }
+
   return (
     <>
-      <Header
-        players={[player1Name || 'Player 1', player2Name || 'Player 2']}
-      />
+      <Header players={[player1Name, player2Name]} />
+      {showPopup && <GameModePopup onSubmit={handleHumanNamesSubmit} />}
       <GameStatus message={message} />
       <BoardComponent grid={grid} onCellClick={handleCellClick} />
     </>
---
+++ src/components/GameModePopup.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+
+type GameModeProps = {
+  onSubmit: (player1Name: string, player2Name: string, isAI: boolean) => void;
+};
+
+const GameModePopup = ({ onSubmit }: GameModeProps) => {
+  const [selectedMode, setSelectedMode] = useState<string>('');
+  const [player1Name, setPlayer1Name] = useState<string>('');
+  const [player2Name, setPlayer2Name] = useState<string>('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!player1Name.trim()) {
+      return;
+    }
+    if (selectedMode === 'Human vs Human' && !player2Name.trim()) {
+      return;
+    }
+    const isAI = selectedMode === 'Human vs Computer';
+    // const isAI = selectedMode;
+    onSubmit(player1Name, isAI ? 'Computer' : player2Name, isAI);
+    // Reset form
+    setSelectedMode('');
+    setPlayer1Name('');
+    setPlayer2Name('');
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white p-4 rounded-lg">
+        <form onSubmit={handleSubmit} className="flex-wrap ">
+          <h1>Choose Game Mode</h1>
+          <div className="p2">
+            <input
+              type="radio"
+              id="humanVsHuman"
+              name="gameMode"
+              value="Human vs Human"
+              checked={selectedMode === 'Human vs Human'}
+              onChange={(e) => setSelectedMode(e.target.value)}
+            />
+            <label htmlFor="humanVsHuman">Human vs Human</label>
+          </div>
+          <div className="">
+            <input
+              type="radio"
+              id="humanVsComputer"
+              name="gameMode"
+              value="Human vs Computer"
+              checked={selectedMode === 'Human vs Computer'}
+              onChange={(e) => setSelectedMode(e.target.value)}
+            />
+            <label htmlFor="humanVsComputer">Human vs Computer</label>
+          </div>
+
+          {/* Conditionally render name inputs */}
+          {selectedMode === 'Human vs Human' && (
+            <div className="rounded-s-md p-8">
+              <input
+                type="text"
+                placeholder="Player 1 Name"
+                value={player1Name}
+                onChange={(e) => setPlayer1Name(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Player 2 Name"
+                value={player2Name}
+                onChange={(e) => setPlayer2Name(e.target.value)}
+              />
+            </div>
+          )}
+          <div className="flex">
+            <button
+              type="submit"
+              className="w-16 rounded-md grow bg-slate-900 text-white hover:bg-slate-400"
+            >
+              Select
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default GameModePopup;
