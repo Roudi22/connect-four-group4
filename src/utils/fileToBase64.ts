bfc258fb1b38b7aa7ec09e3528c9a8893df121c0
Nuur Yusuf <112573333+nyusufdot88@users.noreply.github.com>
2024-09-12 05:20:45 +0200

    Deleted

--- src/utils/fileToBase64.ts
+++
@@ -1,8 +0,0 @@
-export default function fileToBase64(fileInputElement: HTMLInputElement) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL((fileInputElement.files || [])[0]);
-    reader.onerror = () => reject();
-    reader.onload = () => resolve(reader.result);
-  });
-}
