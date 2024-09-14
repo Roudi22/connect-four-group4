fce0501b9c097bcc22a7bae024cfff8274836361
Linus Almgren <linus.almgren@gmail.com>
2024-09-14 21:30:36 +0200

    add sequelize and sqlite for easy user handling

--- typescript/user.ts
+++ typescript/user.ts
@@ -1,5 +1,45 @@
 import express from 'express';
+import { DataTypes, Sequelize } from 'sequelize';
 const userRouter = express.Router();
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './backend/db.sqlite',
+});
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  password: DataTypes.STRING,
+  name: DataTypes.STRING,
+  image: {
+    type: DataTypes.STRING,
+    defaultValue: '',
+  },
+});
+
+try {
+  await sequelize.authenticate();
+  await User.sync();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+try {
+  const newUser = await User.create({
+    username: 'Test',
+    password: '1234',
+    name: 'Tset',
+  });
+  console.log('created new user', newUser);
+} catch (error) {
+  if (error.name === 'SequelizeUniqueConstraintError') {
+    console.log('username already in use');
+  }
+}
 
 userRouter.put('/', (req, res) => {
   // sign in
