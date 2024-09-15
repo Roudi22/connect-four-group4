79d5a692bef7752fb5cdaadd56ed496a2fcdaabe
Linus Almgren <linus.almgren@gmail.com>
2024-09-15 15:01:04 +0200

    add user image upload, delete name from user model and fix some typos

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { createIfNotExists } from 'utils.js';
+import { createIfNotExists } from './utils.js';
 import userRouter from './user.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,18 +16,19 @@ app.use(express.json({ limit: '10MB' }));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.post('/api/uploadImage', (req, res) => {
-  const { player, encodedImage } = req.body;
-  const binaryBuffer = Buffer.from(encodedImage.split('base64,')[1], 'base64');
-  const imagePath = path.join(__dirname, 'images', `player${player}.jpg`);
-
-  fs.writeFile(imagePath, binaryBuffer, (err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Failed to upload image' });
-    }
-    return res.json({ message: 'Image uploaded successfully', imagePath });
-  });
-});
+// old image upload method
+// app.post('/api/uploadImage', (req, res) => {
+//   const { player, encodedImage } = req.body;
+//   const binaryBuffer = Buffer.from(encodedImage.split('base64,')[1], 'base64');
+//   const imagePath = path.join(__dirname, 'images', `player${player}.jpg`);
+//
+//   fs.writeFile(imagePath, binaryBuffer, (err) => {
+//     if (err) {
+//       return res.status(500).json({ error: 'Failed to upload image' });
+//     }
+//     return res.json({ message: 'Image uploaded successfully', imagePath });
+//   });
+// });
 
 app.use('/api/user', userRouter);
 
--- typescript/user.ts
+++ typescript/user.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 import { DataTypes, Model, Sequelize } from 'sequelize';
-import { getHash } from 'utils';
+import { createIfNotExists, getHash } from './utils.js';
 const userRouter = express.Router();
 
 const sequelize = new Sequelize({
@@ -11,7 +13,6 @@ const sequelize = new Sequelize({
 class User extends Model {
   declare username: string;
   declare password: string;
-  declare name: string;
   declare image: string;
 }
 
@@ -22,10 +23,8 @@ User.init(
       unique: true,
     },
     password: DataTypes.STRING,
-    name: DataTypes.STRING,
     image: {
       type: DataTypes.STRING,
-      defaultValue: '',
     },
   },
   { sequelize, modelName: 'User' }
@@ -50,25 +49,29 @@ userRouter.put('/', async (req, res) => {
     return res.json({ msg: 'Wrong username or password' });
   }
 
-  return res.json({ imageUrl: user.image, name: user.name });
+  return res.json({ imageUrl: user.image, name: user.username });
 });
 
 userRouter.post('/', async (req, res) => {
   // sign up
-  const { name, username, password } = req.body;
-  console.log({ name, username, password });
+  const { username, password } = req.body;
+  console.log({ username, password });
 
   try {
+    const imageUrl = path.join(__dirname, 'images', `${username}.jpg`);
+
     const newUser = await User.create({
       username,
       password: getHash(password),
-      name,
+      image: imageUrl,
     });
     console.log(newUser);
 
+    createIfNotExists(`${username}.jpg`);
+
     return res
       .status(201)
-      .json({ name: newUser.name, imageUrl: newUser.image });
+      .json({ name: newUser.username, imageUrl: newUser.image });
   } catch (error) {
     if (error.name === 'SequelizeUniqueConstraintError') {
       return res.json({ msg: 'User already exists' });
@@ -81,10 +84,19 @@ userRouter.post('/', async (req, res) => {
 
 userRouter.put('/image', (req, res) => {
   // post image
-  const { image } = req.body;
+  const { username, image } = req.body;
   console.log({ image });
+  const binaryBuffer = Buffer.from(image.split('base64,')[1], 'base64');
+  const imageUrl = path.join(__dirname, 'images', `${username}.jpg`);
 
-  res.json({ imageUrl: '' });
+  fs.writeFile(imageUrl, binaryBuffer, (error) => {
+    if (error) {
+      return res.status(500).json({ error: 'Failed to upload image' });
+    }
+    return res.json({ msg: 'Image uploaded successfully' });
+  });
+
+  res.json();
 });
 
 export default userRouter;
